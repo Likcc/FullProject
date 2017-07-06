9b4227bec1123380529f5608cef4a1ca24ecb1cf
daniil.ryabov <ryabovdaniil@gmail.com>
2017-07-06 13:39:16 +0700

    Тест с валидными данными (с возможностью изменения и заполнения любого из полей)

--- src/test/java/Page.java
+++ src/test/java/Page.java
@@ -8,7 +8,6 @@ public abstract class Page
 {
     protected String login;
     protected String password;
-    //System.setProperty("webdriver.chrome.driver", "C:\\Users\\daniil.ryabov\\Downloads\\chromedriver_win32\\chromedriver.exe");
     WebDriver driver = new ChromeDriver();
     protected abstract void typeLogPass(String login, String password);
 }
